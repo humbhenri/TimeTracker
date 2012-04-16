20738fb8a122d759d3a5ab781c9b38164871e832
Humberto Pinheiro <humbhenri@gmail.com>
2012-04-16 17:08:11 -0300

    painter save restore

--- projectitemdelegate.cpp
+++ projectitemdelegate.cpp
@@ -10,6 +10,7 @@ ProjectItemDelegate::ProjectItemDelegate(QObject *parent) :
 
 void ProjectItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
+    painter->save();
     if (option.state & QStyle::State_Selected) {
         painter->fillRect(option.rect, option.palette.highlight());
         painter->setBrush(QBrush(Qt::white));
@@ -19,6 +20,7 @@ void ProjectItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &o
     }
     QString projectName = qvariant_cast<QString>(index.data(nameTextRole));
     painter->drawText(option.rect, projectName);
+    painter->restore();
 }
 
 QSize ProjectItemDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
