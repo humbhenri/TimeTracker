5a0682fb6b1f20cb9ea24d74a745f0a801c81817
Humberto Pinheiro <humbhenri@gmail.com>
2012-05-04 16:18:43 -0300

    White text on selected item

--- projectitemdelegate.cpp
+++ projectitemdelegate.cpp
@@ -11,15 +11,19 @@ ProjectItemDelegate::ProjectItemDelegate(QObject *parent) :
 void ProjectItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
     painter->save();
+//    int adjust = -5;
+//    QRect itemRect = option.rect;
+//    itemRect.adjust(adjust, adjust, adjust, adjust);
     if (option.state & QStyle::State_Selected) {
         painter->fillRect(option.rect, option.palette.highlight());
         painter->setBrush(QBrush(Qt::white));
+        painter->setPen(Qt::white);
     }
     else {
         painter->setBrush(QBrush(Qt::black));
     }
     QString projectName = qvariant_cast<QString>(index.data(nameTextRole));
-    painter->drawText(option.rect, projectName);
+    painter->drawText(option.rect.adjusted(5, 5, 5, 5), projectName);
     painter->restore();
 }
 
